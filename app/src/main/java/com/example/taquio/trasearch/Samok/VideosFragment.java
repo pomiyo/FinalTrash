7454b80a5b810f17c2017d2ed811cbff607a3856
Edward Samp <edward.sampayan@gmail.com>
2018-03-16 10:07:40 +0800

    fixinggg!!!!

--- app/src/main/java/com/example/taquio/trasearch/Samok/VideosFragment.java
+++ app/src/main/java/com/example/taquio/trasearch/Samok/VideosFragment.java
@@ -23,49 +23,13 @@ import com.google.firebase.auth.FirebaseAuth;
 public class VideosFragment extends Fragment {
     private static final String TAG = "VideosFragment";
     FirebaseAuth mAuth;
-    private VideoView myPlayer;
-    private MediaController myController;
-    private Uri vidLocation;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_videos, container, false);
         mAuth = FirebaseAuth.getInstance();
 
-//        myPlayer = view.findViewById(R.id.videoView);
-//
-//
-//        myController = new MediaController(getContext());
-//        myController.setAnchorView(myPlayer);
-//        vidLocation = Uri.parse("https://www.youtube.com/watch?v=whFVhvM-J0U");
-//        setUpMedia();
-        Button buttonPlayVideo2 = (Button)view.findViewById(R.id.button1);
-        getActivity().getWindow().setFormat(PixelFormat.UNKNOWN);
-//displays a video file
-        VideoView mVideoView2 = (VideoView)view.findViewById(R.id.videoView1);
-        String uriPath2 = "https://www.youtube.com/watch?v=whFVhvM-J0U";
-        Uri uri2 = Uri.parse(uriPath2);
-        mVideoView2.setVideoURI(uri2);
-        mVideoView2.requestFocus();
-        mVideoView2.start();
-        buttonPlayVideo2.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                VideoView mVideoView2 = (VideoView) view.findViewById(R.id.videoView1);
-// VideoView mVideoView = new VideoView(this);
-                String uriPath = "https://www.youtube.com/watch?v=whFVhvM-J0U";
-                Uri uri2 = Uri.parse(uriPath);
-                mVideoView2.setVideoURI(uri2);
-                mVideoView2.requestFocus();
-                mVideoView2.start();
-            }
-        });
         return view;
-    }
-
-    private void setUpMedia() {
-        myPlayer.setMediaController(myController);
-        myPlayer.setVideoURI(vidLocation);
-        myPlayer.start();
     }
 }
