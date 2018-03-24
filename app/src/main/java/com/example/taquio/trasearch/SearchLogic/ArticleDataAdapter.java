97ff3b29b7e256c4c05180fe9bd6211d50e36479
Roalde Zabate <roaldz13@yahoo.com>
2018-03-24 11:00:08 +0800

    Open Article Links

--- app/src/main/java/com/example/taquio/trasearch/SearchLogic/ArticleDataAdapter.java
+++ app/src/main/java/com/example/taquio/trasearch/SearchLogic/ArticleDataAdapter.java
@@ -1,5 +1,10 @@
 package com.example.taquio.trasearch.SearchLogic;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +21,7 @@ import com.example.taquio.trasearch.R;
 
 public class ArticleDataAdapter extends RecyclerView.Adapter<ArticleDataAdapter.MyViewHolder> {
     private List<ArticleData> articleDataList;
-
+    private Context context;
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView name, description, articleBody;
 
@@ -39,11 +44,50 @@ public class ArticleDataAdapter extends RecyclerView.Adapter<ArticleDataAdapter.
     }
 
     @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        try {
+            context = recyclerView.getContext();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         final ArticleData article = articleDataList.get(position);
         holder.name.setText(article.getName());
         holder.description.setText(article.getDescription());
         holder.articleBody.setText(article.getArticleBody());
+
+        if (!article.getUrl().equalsIgnoreCase("Not Available")) {
+            holder.description.setTextColor(ContextCompat.getColor(context, R.color.colorLink));
+//            holder.name.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary));
+            holder.description.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    String urlSource = "https://www.youtube.com/watch?v=";
+                    String url = article.getUrl();
+//                    String url = urlSource.concat(video.getVideoId());
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    v.getContext().startActivity(i);
+                }
+            });
+        }
+
+        if (!article.getArticleURL().equalsIgnoreCase("Not Available")) {
+            holder.name.setTextColor(ContextCompat.getColor(context, R.color.colorLink));
+            holder.name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String url = article.getArticleURL();
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    v.getContext().startActivity(i);
+                }
+            });
+        }
     }
 
     @Override
