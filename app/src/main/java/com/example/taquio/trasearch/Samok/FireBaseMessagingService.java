7d51e4ba6abca8c732c6c646803d9e5006f78395
Edmar <edmar.greg@gmail.com>
2018-03-26 15:11:59 +0800

    Merge branch 'EdwardBranch' of https://github.com/satisfaction31/FinalTrash into edmar
    # Conflicts:
    #	app/src/main/res/layout/fragment_viewprofile.xml

--- app/src/main/java/com/example/taquio/trasearch/Samok/FireBaseMessagingService.java
+++ app/src/main/java/com/example/taquio/trasearch/Samok/FireBaseMessagingService.java
@@ -2,6 +2,7 @@ package com.example.taquio.trasearch.Samok;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -84,8 +85,7 @@ public class FireBaseMessagingService extends com.google.firebase.messaging.Fire
                 mBuilder.setContentIntent(resultPendingIntent);
 
                 int mNotificationId = (int) System.currentTimeMillis();
-                NotificationManager mNotifyMgr =
-                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                NotificationManager mNotifyMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 mNotifyMgr.notify(mNotificationId, mBuilder.build());
             }
 
